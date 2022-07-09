9f4e7aae6fbee768db74e2f63e70edd90e472ae7
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-07-09 21:59:42 +0200

    Add document data model

---
+++ src/app/models/document.model.ts
@@ -0,0 +1,27 @@
+type Visibility = 'public' | 'private';
+
+export interface iDocument {
+    _id?: string;
+    title: string;
+    content: Array<{
+        text: string;
+        options: Array<{ key: string; value: string | number }>;
+    }>;
+    keywords: Array<string>;
+    author: string;
+    fork?: string;
+    visibility: Visibility;
+}
+
+export class Document implements iDocument {
+    constructor(
+        public title: string,
+        public content: Array<{
+            text: string;
+            options: Array<{ key: string; value: string | number }>;
+        }>,
+        public keywords: Array<string>,
+        public author: string,
+        public visibility: Visibility
+    ) {}
+}
