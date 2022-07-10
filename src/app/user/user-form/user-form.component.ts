eaa91cfddee1fa6e316c9877cefe92a16405b16e
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-07-11 00:45:44 +0200

    Upgrade user component and form

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { iUser } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent implements OnInit {
+  currentUser!: iUser;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
