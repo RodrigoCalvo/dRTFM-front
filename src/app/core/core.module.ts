bb25fbb58d5ff675cc4f19cbf9802d691005709d
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-07-11 00:43:18 +0200

    Add basic logo component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     LogoComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
