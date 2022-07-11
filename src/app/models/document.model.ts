26810efff250774d7e5654574bb9c0c1e1504956
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-11 13:06:55 +0200

    Add store state interfaces

--- src/app/models/document.model.ts
+++ src/app/models/document.model.ts
@@ -1,27 +1,30 @@
 type Visibility = 'public' | 'private';
 
+export interface iDocumentsState {
+  documents: ReadonlyArray<iDocument>;
+}
 export interface iDocument {
-    _id?: string;
-    title: string;
-    content: Array<{
-        text: string;
-        options: Array<{ key: string; value: string | number }>;
-    }>;
-    keywords: Array<string>;
-    author: string;
-    fork?: string;
-    visibility: Visibility;
+  _id?: string;
+  title: string;
+  content: Array<{
+    text: string;
+    options: Array<{ key: string; value: string | number }>;
+  }>;
+  keywords: Array<string>;
+  author: string;
+  fork?: string;
+  visibility: Visibility;
 }
 
 export class Document implements iDocument {
-    constructor(
-        public title: string,
-        public content: Array<{
-            text: string;
-            options: Array<{ key: string; value: string | number }>;
-        }>,
-        public keywords: Array<string>,
-        public author: string,
-        public visibility: Visibility
-    ) {}
+  constructor(
+    public title: string,
+    public content: Array<{
+      text: string;
+      options: Array<{ key: string; value: string | number }>;
+    }>,
+    public keywords: Array<string>,
+    public author: string,
+    public visibility: Visibility
+  ) {}
 }
