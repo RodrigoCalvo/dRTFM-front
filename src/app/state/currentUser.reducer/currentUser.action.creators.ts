97f271eb197924a7dee63c7c73471d2206932e2d
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-11 13:05:40 +0200

    Add currentUser actions creators & reducer

---
+++ src/app/state/currentUser.reducer/currentUser.action.creators.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from '@ngrx/store';
+import { iUser } from 'src/app/models/user.model';
+
+export const loadCurrentUser = createAction(
+  '[CurrentUser Object] Load CurrentUser',
+  props<{ currentUser: iUser }>()
+);
