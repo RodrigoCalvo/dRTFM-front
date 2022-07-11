af8139b6d39b23c9d0fed9d17981ffbd624c27ee
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-11 13:25:27 +0200

    Add Search module routing & bar data dependency

--- src/app/search/search-bar/search-bar.component.ts
+++ src/app/search/search-bar/search-bar.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { iDocument } from 'src/app/models/document.model';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
+  searchArray!: Array<iDocument>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
