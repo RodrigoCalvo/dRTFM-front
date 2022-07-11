96eb684541622184040eb6b031494df7c1897f5f
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-11 13:23:27 +0200

    Add RouterModule dependency

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    LogoComponent,
-    HeaderComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LogoComponent, HeaderComponent, NotFoundComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, LogoComponent, NotFoundComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
