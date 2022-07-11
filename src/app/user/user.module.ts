b94e854bbfcdbbb04e06c6fdb07535df29fa7b5b
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-11 13:25:45 +0200

    Add User module routing

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { UserFormComponent } from './user-form/user-form.component';
-
-
+import { CoreModule } from '../core/core.module';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
-  declarations: [
-    UserComponent,
-    UserFormComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserComponent, UserFormComponent],
+  imports: [CommonModule, CoreModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
