d1b5b0fef5e9fd2ed3557ce67b11d6f7fa5752df
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-12 17:22:01 +0200

    Update login form functionality

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { LocalStorageService } from 'src/app/services/local.storage.service';
 import { UsersApiService } from 'src/app/services/users.api.service';
+import { AppState } from 'src/app/state/app.state';
 import { loadCurrentUser } from 'src/app/state/currentUser.reducer/currentUser.action.creators';
 
 @Component({
@@ -10,28 +13,35 @@ import { loadCurrentUser } from 'src/app/state/currentUser.reducer/currentUser.a
 })
 export class LoginFormComponent implements OnInit {
   loginData!: { email: string; password: string };
-  constructor(private userApi: UsersApiService, private store: Store) {}
+  loginError!: boolean;
+  constructor(
+    private userApi: UsersApiService,
+    private store: Store<AppState>,
+    private router: Router,
+    private localStorage: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
     this.loginData = { email: '', password: '' };
+    this.loginError = false;
   }
 
   handleSubmit() {
     if (this.loginData.email && this.loginData.password) {
-      try {
-        this.userApi
-          .loginUser(this.loginData.email, this.loginData.password)
-          .subscribe({
-            next: (data) => {
-              if (data.token) {
-                this.store.dispatch(
-                  loadCurrentUser({ currentUser: data.user, token: data.token })
-                );
-                console.log(data);
-              }
-            },
-          });
-      } catch (e) {}
+      this.userApi.loginUser(this.loginData).subscribe({
+        next: (data) => {
+          if (data.token) {
+            this.store.dispatch(
+              loadCurrentUser({ currentUser: data.user, token: data.token })
+            );
+            this.localStorage.saveToken(data.token);
+            this.router.navigate(['home']);
+          }
+        },
+        error: (err) => {
+          this.loginError = true;
+        },
+      });
     }
   }
 }
