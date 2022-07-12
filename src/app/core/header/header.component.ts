e6f9460e3e55962b22c5d7e97adaefa4b2fe9268
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-12 17:20:43 +0200

    Add login button to header, w/ functionality

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  logged!: boolean;
+  constructor(private store: Store<AppState>, private router: Router) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.currentUser)
+      .subscribe({ next: (data) => (this.logged = Boolean(data.token)) });
   }
 
+  goLogin() {
+    this.router.navigate(['login']);
+  }
 }
