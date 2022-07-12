09df3136a7bd991c80410bb8f895bac7c39233f3
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-12 16:29:31 +0200

    Add preload token to app onInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { iUser } from './models/user.model';
+import { LocalStorageService } from './services/local.storage.service';
+import { AppState } from './state/app.state';
+import { loadCurrentUser } from './state/currentUser.reducer/currentUser.action.creators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drtfm';
+  constructor(
+    private localStorage: LocalStorageService,
+    private store: Store<AppState>
+  ) {}
+  ngOnInit(): void {
+    let token = this.localStorage.getToken();
+    if (token)
+      this.store.dispatch(loadCurrentUser({ currentUser: {} as iUser, token }));
+  }
 }
