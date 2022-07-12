e084da70b312ddccfdda4589b610a684ba35791a
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-12 13:42:24 +0200

    Extra: Add StoreDevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -15,6 +17,11 @@ import { ROOT_REDUCERS } from './state/app.state';
     CoreModule,
     HttpClientModule,
     StoreModule.forRoot(ROOT_REDUCERS),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
