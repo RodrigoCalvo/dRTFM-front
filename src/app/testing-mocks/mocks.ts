f634bd7fa08a8bfc1d741008875d049ae3fb05e6
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-13 09:59:42 +0200

    Add mocks file for imports

---
+++ src/app/testing-mocks/mocks.ts
@@ -0,0 +1,14 @@
+export const mockInitialState = {
+  documents: { documents: [] },
+  currentUser: {
+    user: {
+      name: '',
+      email: '',
+      password: '',
+      photo: '',
+      myDocuments: [],
+      myFavs: [],
+    },
+    token: '',
+  },
+};
