65bd9d247d091138d66e4c3029bc901ada0ba9e4
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-14 17:42:04 +0200

    Add accordion menu toggle tests in favourites menu

--- src/app/favourites/favourites-menu/favourites-menu.component.spec.ts
+++ src/app/favourites/favourites-menu/favourites-menu.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { mockInitialState } from 'src/app/testing-mocks/mocks';
 
 import { FavouritesMenuComponent } from './favourites-menu.component';
 
@@ -8,9 +10,9 @@ describe('FavouritesMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FavouritesMenuComponent ]
-    })
-    .compileComponents();
+      declarations: [FavouritesMenuComponent],
+      providers: [provideMockStore({ initialState: mockInitialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FavouritesMenuComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,19 @@ describe('FavouritesMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('When calling component.toggleDocs', () => {
+    it('should change component.openedMyDocs and set openedMyFavs to false', () => {
+      component.toggleDocs();
+      expect(component.openedMyDocs).toBeTrue();
+      expect(component.openedMyFavs).toBeFalse();
+    });
+  });
+  describe('When calling component.toggleFavs', () => {
+    it('should change component.openedMyFavs and set openedMyDocs to false', () => {
+      component.toggleFavs();
+      expect(component.openedMyFavs).toBeTrue();
+      expect(component.openedMyDocs).toBeFalse();
+    });
   });
 });
