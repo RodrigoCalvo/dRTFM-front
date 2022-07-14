0a74b754b6e85fa69d866ac805050cec52077af3
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-14 17:40:12 +0200

    Add error control test to app preload currentUser

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,11 +3,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { provideMockStore } from '@ngrx/store/testing'; //manual import
 import { mockInitialState } from './testing-mocks/mocks';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
+import { iUser } from './models/user.model';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AppComponent],
       providers: [provideMockStore({ initialState: mockInitialState })],
     }).compileComponents();
@@ -30,10 +33,30 @@ describe('AppComponent', () => {
       const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.componentInstance;
       spyOn(app.localStorage, 'getToken').and.returnValue('token');
+      spyOn(app.usersApi, 'loginUser').and.returnValue(
+        of({ user: {} as iUser, token: '' })
+      );
       spyOn(app.store, 'dispatch');
       fixture.detectChanges();
 
       expect(app.store.dispatch).toHaveBeenCalled();
+    });
+  });
+  describe('When app is loaded with invalid token in localStorage', () => {
+    it('should uselocalStorage.clearToken to delete the saved token', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      spyOn(app.localStorage, 'getToken').and.returnValue('token');
+      spyOn(app.usersApi, 'loginUser').and.returnValue(
+        new Observable(() => {
+          throw new Error();
+        })
+      );
+
+      spyOn(app.localStorage, 'clearToken');
+      fixture.detectChanges();
+
+      expect(app.localStorage.clearToken).toHaveBeenCalled();
     });
   });
   describe('When app is loaded without token in localStorage', () => {
