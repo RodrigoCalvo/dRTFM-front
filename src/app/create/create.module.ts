8365827fb9799ff6247292b3a750e5ce4f2eee0e
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-14 13:47:15 +0200

    Update create module dependencies

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -4,16 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 import { CreateFormComponent } from './create-form/create-form.component';
-
+import { CoreModule } from '../core/core.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    CreateComponent,
-    CreateFormComponent
-  ],
-  imports: [
-    CommonModule,
-    CreateRoutingModule
-  ]
+  declarations: [CreateComponent, CreateFormComponent],
+  imports: [CommonModule, CreateRoutingModule, CoreModule, FormsModule],
 })
-export class CreateModule { }
+export class CreateModule {}
