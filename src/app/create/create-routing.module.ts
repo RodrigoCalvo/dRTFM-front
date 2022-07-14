8a2012e14f795cecfbd43302cfababfdcedc622b
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-14 13:45:13 +0200

    Update create module routing

--- src/app/create/create-routing.module.ts
+++ src/app/create/create-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: CreateComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CreateRoutingModule { }
+export class CreateRoutingModule {}
