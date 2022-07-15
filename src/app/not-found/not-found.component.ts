26cc6c986ecd38d93b108d4489e58ad3836f1863
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-15 13:47:19 +0200

    Add notFound functionallity

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss'],
+})
+export class NotFoundComponent implements OnInit {
+  speed!: number;
+  currentClass!: string;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.speed = 1;
+    this.currentClass = 'imgr' + this.speed;
+  }
+
+  goBackFake() {
+    this.speed++;
+    if (this.currentClass !== 'imgr7') this.currentClass = 'imgr' + this.speed;
+  }
+}
