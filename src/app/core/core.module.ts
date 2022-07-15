c87f5fb75c5b17f159950bec598870f964a6a224
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-15 13:45:53 +0200

    Delete old module dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [LogoComponent, HeaderComponent, NotFoundComponent],
+  declarations: [LogoComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, LogoComponent, NotFoundComponent],
+  exports: [HeaderComponent, LogoComponent],
 })
 export class CoreModule {}
