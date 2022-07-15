ac4c94fa880996c4f10e4a800cd513bf9fb17946
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-15 12:02:41 +0200

    Add user form func: preload, submit, cancel, edit

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,10 +1,16 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { iUser } from 'src/app/models/user.model';
+import { iCurrentUserState, iUser } from 'src/app/models/user.model';
 import { LocalStorageService } from 'src/app/services/local.storage.service';
+import { UsersApiService } from 'src/app/services/users.api.service';
 import { AppState } from 'src/app/state/app.state';
-import { clearCurrentUser } from 'src/app/state/currentUser.reducer/currentUser.action.creators';
+import {
+  clearCurrentUser,
+  loadCurrentUser,
+} from 'src/app/state/currentUser.reducer/currentUser.action.creators';
+import { isEmail } from '../../helpers/isEmail';
 
 @Component({
   selector: 'app-user-form',
@@ -12,14 +18,100 @@ import { clearCurrentUser } from 'src/app/state/currentUser.reducer/currentUser.
   styleUrls: ['./user-form.component.scss'],
 })
 export class UserFormComponent implements OnInit {
-  currentUser: iUser = { name: 'test', email: 'tes@test.com' } as iUser; //data demo
+  currentUserData!: iCurrentUserState;
+  formData!: {
+    name: string;
+    email: string;
+    password: string;
+    rPassword: string;
+  };
+  editable!: boolean;
+  errorMessage!: string;
   constructor(
     public store: Store<AppState>,
+    public userApi: UsersApiService,
     public localStorage: LocalStorageService,
     public router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store
+      .select((state) => state.currentUser)
+      .subscribe({
+        next: (data) => (this.currentUserData = data),
+      });
+    this.editable = false;
+    this.formData = {
+      name: this.currentUserData.user.name,
+      email: this.currentUserData.user.email,
+      password: '',
+      rPassword: '',
+    };
+    this.errorMessage = '';
+  }
+
+  handleSubmit() {
+    if (isEmail(this.formData.email)) {
+      if (this.formData.password === this.formData.rPassword) {
+        if (
+          this.formData.name === this.currentUserData.user.name &&
+          this.formData.email === this.currentUserData.user.email &&
+          !this.formData.password
+        ) {
+          // no changes
+          this.editable = false;
+        } else {
+          const updateUser: Partial<iUser> = {
+            name: this.formData.name
+              ? this.formData.name
+              : this.currentUserData.user.name,
+            email: this.formData.email
+              ? this.formData.email
+              : this.currentUserData.user.email,
+          };
+          if (this.formData.password)
+            updateUser.password = this.formData.password;
+          this.userApi
+            .updateUser(updateUser, this.currentUserData.token)
+            .subscribe({
+              next: (data) => {
+                if (data._id) {
+                  this.store.dispatch(
+                    loadCurrentUser({
+                      currentUser: data,
+                      token: this.currentUserData.token,
+                    })
+                  );
+                  this.editable = false;
+                  this.currentUserData.user = data;
+                }
+              },
+              error: (err) => {
+                this.errorMessage = 'Error al actualizar el usuario.';
+              },
+            });
+        }
+      } else {
+        this.errorMessage = 'Las contraseñas no coinciden.';
+      }
+    } else {
+      this.errorMessage = 'El email es incorrecto.';
+    }
+  }
+
+  toggleEditable() {
+    this.editable = !this.editable;
+  }
+
+  handleCancel() {
+    this.toggleEditable();
+    this.formData = {
+      name: this.currentUserData.user.name,
+      email: this.currentUserData.user.email,
+      password: '',
+      rPassword: '',
+    };
+  }
 
   logout() {
     this.localStorage.clearToken();
