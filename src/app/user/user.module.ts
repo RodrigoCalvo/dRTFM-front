89d62b7c8caa56f258562a7bd1c591a8cb7b6c5e
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-15 12:03:15 +0200

    Add user module dependencies

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserComponent } from './user.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { CoreModule } from '../core/core.module';
 import { UserRoutingModule } from './user-routing.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UserComponent, UserFormComponent],
-  imports: [CommonModule, CoreModule, UserRoutingModule],
+  imports: [CommonModule, CoreModule, UserRoutingModule, FormsModule],
 })
 export class UserModule {}
