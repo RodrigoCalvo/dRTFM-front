8b30ae331bf0e734cb8f27ef6e1977adbb5c4370
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-16 19:40:24 +0200

    Extra: Add confirm dialog files

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 @NgModule({
-  declarations: [LogoComponent, HeaderComponent],
+  declarations: [LogoComponent, HeaderComponent, ConfirmDialogComponent],
   imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, LogoComponent],
 })
