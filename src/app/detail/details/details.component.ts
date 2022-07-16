e7f696065c828e94aa0a1622cbdc864d404f70f7
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-16 17:54:47 +0200

    Update details component functionality

--- src/app/detail/details/details.component.ts
+++ src/app/detail/details/details.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { iDocument } from 'src/app/models/document.model';
+import { DocumentsApiService } from 'src/app/services/documents.api.service';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
   selector: 'app-details',
@@ -7,13 +11,65 @@ import { iDocument } from 'src/app/models/document.model';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  @Input() document: iDocument = {
-    title: 'test',
-    keywords: [''],
-    author: { _id: '', name: 'test' },
-    fork: '',
-  } as iDocument; // data demo
-  constructor() {}
+  documentId!: string;
+  document!: iDocument;
+  errorMessage!: string;
+  isMine!: boolean;
+  editEnable!: boolean;
+  constructor(
+    public documentApi: DocumentsApiService,
+    public store: Store<AppState>,
+    public route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.documentId = this.route.snapshot.paramMap.get('id') as string;
+    this.errorMessage = 'Cargando...';
+    this.isMine = false;
+    this.editEnable = false;
+    this.store
+      .select((state) => state.documents)
+      .subscribe({
+        next: (data) => {
+          const docFind = data.documents.find(
+            (doc) => doc._id === this.documentId
+          );
+          if (docFind) {
+            this.document = docFind;
+            this.errorMessage = '';
+            this.store
+              .select((state) => state.currentUser)
+              .subscribe({
+                next: (data) =>
+                  (this.isMine = data.user._id === this.document.author._id),
+              });
+          } else {
+            this.documentApi.getDocument(this.documentId).subscribe({
+              next: (data) => {
+                this.document = data;
+                this.errorMessage = '';
+                this.store
+                  .select((state) => state.currentUser)
+                  .subscribe({
+                    next: (data) =>
+                      (this.isMine =
+                        data.user._id === this.document.author._id),
+                  });
+              },
+              error: (err) => (this.errorMessage = 'Documento no encontrado.'),
+            });
+          }
+        },
+      });
+  }
+
+  enableEdit() {
+    this.editEnable = true;
+  }
+  handleCancel() {
+    this.editEnable = false;
+  }
+  handleFork() {}
+  handleSubmit() {}
+  handleDelete() {}
 }
