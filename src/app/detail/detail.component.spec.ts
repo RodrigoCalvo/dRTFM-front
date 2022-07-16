6dcaeb8e76df752ec1892d55044d1c69a44171e2
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-16 18:00:58 +0200

    Update test dependencies

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -15,7 +16,7 @@ describe('DetailComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [DetailComponent, HeaderComponent, DetailsComponent],
       providers: [provideMockStore({ initialState: mockInitialState })],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DetailComponent);
