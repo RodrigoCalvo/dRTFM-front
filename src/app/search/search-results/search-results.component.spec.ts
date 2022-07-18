7d8da49578de0a3b5b1e188ca143d1e60980e5e8
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-18 23:12:56 +0200

    Fix search result component test dependencies

--- src/app/search/search-results/search-results.component.spec.ts
+++ src/app/search/search-results/search-results.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { mockInitialState } from 'src/app/testing-mocks/mocks';
 
 import { SearchResultsComponent } from './search-results.component';
 
@@ -8,9 +12,10 @@ describe('SearchResultsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchResultsComponent ]
-    })
-    .compileComponents();
+      declarations: [SearchResultsComponent],
+      providers: [provideMockStore({ initialState: mockInitialState })],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchResultsComponent);
     component = fixture.componentInstance;
