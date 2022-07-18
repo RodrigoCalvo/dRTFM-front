ea96d31e734bfcabed375c4b6089ae36852e48be
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-18 19:31:51 +0200

    Update searchbar w/ non-searched results

--- src/app/search/search-bar/search-bar.component.ts
+++ src/app/search/search-bar/search-bar.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { BehaviorSubject, debounceTime } from 'rxjs';
 import { iDocument } from 'src/app/models/document.model';
 import { DocumentsApiService } from 'src/app/services/documents.api.service';
+import { AppState } from 'src/app/state/app.state';
+import { loadDocuments } from 'src/app/state/documents.reducer/documents.action.creators';
 
 @Component({
   selector: 'app-search-bar',
@@ -12,13 +15,21 @@ import { DocumentsApiService } from 'src/app/services/documents.api.service';
 export class SearchBarComponent implements OnInit {
   search = new FormControl('');
   searchArray!: Array<iDocument>;
+  topDocuments!: Array<iDocument>;
   delayMiddleware$: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor(public documentApi: DocumentsApiService) {
+  constructor(
+    public documentApi: DocumentsApiService,
+    public store: Store<AppState>
+  ) {
     this.delayMiddleware$.pipe(debounceTime(500)).subscribe({
       next: (search) => {
         documentApi.searchDocument(search).subscribe({
           next: (data) => {
+            if (!data) {
+              this.loadTopDocuments();
+              return;
+            }
             this.searchArray = data;
           },
         });
@@ -26,9 +37,21 @@ export class SearchBarComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadTopDocuments();
+  }
 
   sendSearch() {
     this.delayMiddleware$.next(this.search.value as string);
+  }
+
+  loadTopDocuments() {
+    if (!(this.search.value && this.search.value.length > 2)) {
+      this.documentApi.getDocuments().subscribe({
+        next: (data) => {
+          this.searchArray = [...data];
+        },
+      });
+    }
   }
 }
