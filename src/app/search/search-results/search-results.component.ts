5b9340851c422f2abee470d16c4b44c8d1f15c4d
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-18 15:36:54 +0200

    Add search results icon functionality (no button)

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { iDocument } from 'src/app/models/document.model';
+import { iCurrentUserState } from 'src/app/models/user.model';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
   selector: 'app-search-results',
@@ -8,7 +11,37 @@ import { iDocument } from 'src/app/models/document.model';
 })
 export class SearchResultsComponent implements OnInit {
   @Input() searchArray!: Array<iDocument>;
-  constructor() {}
+  currentUserData!: iCurrentUserState;
+  constructor(public store: Store<AppState>) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store
+      .select((state) => state.currentUser)
+      .subscribe({
+        next: (data) => (this.currentUserData = data),
+      });
+  }
+
+  checkIfItIs(type: string, docId: string | undefined): boolean {
+    switch (type) {
+      case 'fav':
+        return Boolean(
+          this.currentUserData.user.myFavs.find((item) => item._id === docId)
+        );
+      case 'mine':
+        return Boolean(
+          this.currentUserData.user.myDocuments.find(
+            (item) => item._id === docId
+          )
+        );
+      case 'fork':
+        return Boolean(
+          this.currentUserData.user.myDocuments.find(
+            (item) => item.fork === docId
+          )
+        );
+      default:
+        return false;
+    }
+  }
 }
