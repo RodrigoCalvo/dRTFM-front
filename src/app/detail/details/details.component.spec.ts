3d31dd5247c4519ecd9c33b7544d0ecb27c4fbe7
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-19 13:02:38 +0200

    Update details component tests

--- src/app/detail/details/details.component.spec.ts
+++ src/app/detail/details/details.component.spec.ts
@@ -1,13 +1,47 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { of } from 'rxjs';
-import { iDocument } from 'src/app/models/document.model';
-import { iUser } from 'src/app/models/user.model';
-import { mockInitialState } from 'src/app/testing-mocks/mocks';
+import { iDocument } from '../../models/document.model';
+import { iCurrentUserState, iUser } from '../../models/user.model';
+import { AppState } from '../../state/app.state';
+import { mockInitialState } from '../../testing-mocks/mocks';
 
 import { DetailsComponent } from './details.component';
+
+const mockDocument: iDocument = {
+  _id: 'test1',
+  title: 'test1',
+  content: [],
+  keywords: [],
+  author: {
+    _id: 'userId',
+    name: '',
+  },
+  visibility: 'public',
+};
+
+const fullMockInitialState: AppState = {
+  documents: {
+    documents: [
+      //mockDocument
+    ],
+  },
+  currentUser: {
+    user: {
+      _id: 'userId',
+      name: '',
+      email: '',
+      password: '',
+      photo: '',
+      myDocuments: [],
+      myFavs: [],
+    },
+    token: '',
+  },
+};
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
@@ -16,7 +50,19 @@ describe('DetailsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DetailsComponent],
-      providers: [provideMockStore({ initialState: mockInitialState })],
+      providers: [
+        provideMockStore({ initialState: fullMockInitialState }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({
+                id: 'test1',
+              }),
+            },
+          },
+        },
+      ],
       imports: [RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
@@ -73,9 +119,39 @@ describe('DetailsComponent', () => {
     });
   });
 
-  // describe('When calling component.handleSubmit', () => {
+  describe('When calling component.handleSubmit', () => {
+    it('should call store.dispatch twice', () => {
+      const mockDocument: iDocument = {
+        title: '',
+        author: { _id: '', name: '' },
+        content: [{ text: '', options: [{ key: '', value: '' }] }],
+        keywords: ['', ''],
+        visibility: 'public',
+      };
+      const mockUserData: iCurrentUserState = {
+        user: {} as iUser,
+        token: 'token',
+      };
+      component.documentData = {
+        title: 'test',
+        contentString: 'test',
+        keywordsString: 'test',
+      };
+      component.document = mockDocument;
+      component.currentUserData = mockUserData;
 
-  // });
+      spyOn(component.documentApi, 'updateDocument').and.returnValue(
+        of(mockDocument)
+      );
+      spyOn(component.store, 'dispatch');
+      spyOn(component.usersApi, 'loginUser').and.returnValue(of(mockUserData));
+      fixture.detectChanges();
+
+      component.handleSubmit();
+
+      expect(component.store.dispatch).toHaveBeenCalledTimes(2);
+    });
+  });
 
   describe('When calling component.handleDelete', () => {
     it('should set component.showDeletePrompt to false', () => {
